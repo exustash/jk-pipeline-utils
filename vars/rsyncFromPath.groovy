1630dee5e4e87f39121c2aa122c4dd4a0fc871e8
exuper o <exuper.dev@gmail.com>
2018-09-17 13:26:06 +0200

    refactor(rsync): implements variable remote host

--- vars/rsyncFromPath.groovy
+++ vars/rsyncFromPath.groovy
@@ -5,12 +5,12 @@
  * @param localPath
  * @param remotePath
  */
-Void call(String localPath, String remotePath) {
+Void call(String remoteHost, String localPath, String remotePath) {
     try {
         println "---> Download from ${remotePath} to ${localPath} ####"
         sh returnStdout: false, script: "mkdir -p /tmp/${localPath}"
         sh returnStdout: false, script: "rm -rf /tmp/${localPath}/*"
-        sh returnStdout: false, script: "rsync --exclude .git -a install.ear.th::${remotePath}/ /tmp/${localPath}/"
+        sh returnStdout: false, script: "rsync --exclude .git -a ${remoteHost}::${remotePath}/ /tmp/${localPath}/"
     } catch (err) {
         error("[ERR!] Pipeline execution error: ${err.message}")
     }
