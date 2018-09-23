8628169ed4ad7b66de04efcfa330271ec234ee1f
exuper o <exuper.dev@gmail.com>
2018-09-23 18:06:55 +0200

    refactor(runContaineeStack): introduce override for compose as a params

--- vars/runContainerStack.groovy
+++ vars/runContainerStack.groovy
@@ -5,11 +5,11 @@
  * @param  serviceName       [description]
  * @return                   [description]
  */
- Void call(String serviceName) {
+ Void call(String stackName, override='') {
      reportBuildStatus("release/pipeline/stack.build", "Pipeline Task stack.build is runing", 'PENDING')
      try {
          println "---> Building ${serviceName} Docker image ${serviceName}"
-         sh(returnStdout: false, script: "docker-compose -f docker-compose.yml build --no-cache")
+         sh(returnStdout: false, script: "docker-compose -f docker-compose.yml ${override} run")
          reportBuildStatus("release/pipeline/stack.build", "Pipeline Task stack.build has succeeded", 'SUCCESS')
      } catch (err) {
         reportBuildStatus("release/pipeline/stack.build", "Pipeline Task stack.build has failed", 'FAILURE')
