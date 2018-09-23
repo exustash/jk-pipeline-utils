268c5c6db6cb818144dd184a2aea72a279c8fab8
exuper o <exuper.dev@gmail.com>
2018-09-23 18:08:25 +0200

    refactor(runContaineeStack): remove unnecessary report to GH PR

--- vars/runContainerStack.groovy
+++ vars/runContainerStack.groovy
@@ -6,13 +6,10 @@
  * @return                   [description]
  */
  Void call(String stackName, override='') {
-     reportBuildStatus("release/pipeline/stack.build", "Pipeline Task stack.build is runing", 'PENDING')
      try {
-         println "---> Building ${serviceName} Docker image ${serviceName}"
+         println "---> Running ${stackName}"
          sh(returnStdout: false, script: "docker-compose -f docker-compose.yml ${override} run")
-         reportBuildStatus("release/pipeline/stack.build", "Pipeline Task stack.build has succeeded", 'SUCCESS')
      } catch (err) {
-        reportBuildStatus("release/pipeline/stack.build", "Pipeline Task stack.build has failed", 'FAILURE')
         error("[ERR!] Pipeline execution error: ${err.message}")
      }
  }
