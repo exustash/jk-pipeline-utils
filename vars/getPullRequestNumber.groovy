9f226cce3ae7e2ce9c6ee686d75cc17ea486aab7
Exuper O <exuper.dev@gmail.com>
2018-11-21 07:25:02 +0100

    feat: adds git operations steps

--- vars/getPullRequestNumber.groovy
+++ vars/getPullRequestNumber.groovy
@@ -1,7 +1,7 @@
 /**
 * Retrieve the current pull request number
 */
-Integer getPullRequestNumber() {
+Integer call() {
     env.CHANGE_ID != null && env.CHANGE_ID.length() > 0
     return env.CHANGE_ID
 }
--- vars/git.groovy
+++ vars/git.groovy
@@ -2,50 +2,6 @@
 
 
 /**
- * Get the short format commit hash of the current commit
- */
-String getCommitHash() {
-  return sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-}
-
-/**
- * Get the git message of the current commit
- */
-String getCommitMsg() {
-  return sh(returnStdout: true, script: "giat log -n 1 --pretty=format:'%s'").trim()
-}
-
-/**
- * Get the current branch name
- */
-String getCurrentBranch() {
-  String currentBranch = env.BRANCH_NAME
-  if (!currentBranch) {
-    currentRaw = sh(script: 'git name-rev --name-only HEAD', returnStdout: true).trim()
-    currentBranch = currentRaw.split('remotes/origin/')[1];
-  }
-  return currentBranch
-}
-
-/**
- * retrieve the url of the repositiry associated with current build
- * @return an url string
- */
-String getRepositoryURL() {
-  sh "git config --get remote.origin.url > .git/remote-url"
-  return readFile(".git/remote-url").trim()
-}
-
-/**
-* Retrieve the current pull request number
-*/
-Integer getPullRequestNumber() {
-    env.CHANGE_ID != null && env.CHANGE_ID.length() > 0
-    return env.CHANGE_ID
-}
-
-
-/**
  * Get the email of the author of the current commit
  */
 String getAuthorEmail() {
