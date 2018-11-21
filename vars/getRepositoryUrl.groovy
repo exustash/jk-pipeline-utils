1e9d5bdad56c87dfad06ae69b590fbda8446540b
Exuper O <exuper.dev@gmail.com>
2018-11-21 07:25:39 +0100

    feat: adds git operations steps

--- vars/getRepositoryUrl.groovy
+++ vars/getRepositoryUrl.groovy
@@ -2,7 +2,7 @@
  * retrieve the url of the repositiry associated with current build
  * @return an url string
  */
-String getRepositoryURL() {
+String call() {
   sh "git config --get remote.origin.url > .git/remote-url"
   return readFile(".git/remote-url").trim()
 }
