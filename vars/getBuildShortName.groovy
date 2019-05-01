4a4f562e0171c4d405c87f633856e2e567b3f06c
Exuper O <exuper.dev@gmail.com>
2019-05-01 16:24:10 +0200

    refactor(buildshortname): adds setp to retrieve a short name for every build

---
+++ vars/getBuildShortName.groovy
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env groovy
+import java.net.URLDecoder
+
+/**
+ * Create a jenkins based build tag
+ * @param numberOfDirectories
+ */
+ def call(int numberOfDirectories = 2) {
+  // Jenkins double encode '/' character in URL
+  def decodedJobUrl = URLDecoder.decode(env.JOB_URL, "UTF-8")
+  decodedJobUrl = URLDecoder.decode(decodedJobUrl, "UTF-8")
+  parts = decodedJobUrl.replaceAll("/", "").split('job')
+
+  if(parts.size() > 1) {
+    numberOfDirectories = Math.min(numberOfDirectories, parts.size())
+    parts = parts.takeRight(numberOfDirectories)
+  }
+
+  return parts.join('-')
+}
